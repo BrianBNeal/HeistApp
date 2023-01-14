b83379705c29288af40c3bd5a2e8ffa1ea5122b0
Brian Neal <brianbneal@gmail.com>
2023-01-14 15:59:44 -0600

    Add project files.

---
+++ HeistConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Phase 1
+    1. Print the message "Plan Your Heist!".
+    2. Create a way to store a single team member. A team member will have a *name*, a *skill Level* and a *courage factor*. The *skill Level* will be a positive integer and the *courage factor* will be a decimal between 0.0 and 2.0.
+    3. Prompt the user to enter a team member's name and save that name.
+    4. Prompt the user to enter a team member's skill level and save that skill level with the name.
+    5. Prompt the user to enter a team member's courage factor and save that courage factor with the name.
+    6. Display the team member's information.
+*/
+
+//make some notes for a potential plan of action
+
+/* -make a team member and print it
+ *   -make a TeamMember class, including a ToString()
+ * -gonna need to validate the inputs are correct (string, int, decimal)
+ *   -validation methods located in TeamMember class?  No, before the creation of the object, so during input.
+ * -maybe a TeamMemberFactory class to handle all inquiries and validation?
+ */
